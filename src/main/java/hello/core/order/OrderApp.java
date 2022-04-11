b8321c39cdb94d3b2c2f78801be0b7522bd8b418
skykchmin <skykchmin@naver.com>
2022-04-11 23:55:38 +0900

    [220411] feat - 할인정책 적용

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -5,7 +5,7 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 
-public class OrderAop {
+public class OrderApp {
 
     public static void main(String[] args) {
         MemberService memberService = new MemberServiceImpl();
